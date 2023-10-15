f98b1a4707e479ff2def5e8b2e6c72105a245b77
SarahMohammadiNejad <sarah.mohammadinejad@gmail.com>
2023-10-15 16:09:49 +0200

    saved top movie df, faster?

--- choice_of_method_to_recommend.py
+++ choice_of_method_to_recommend.py
@@ -101,7 +101,9 @@ class MovieRecommendation:
                 for ii in user_unseen_movies:    
 
                     rate=recommendations_reconstructed.loc[target_user,ii]
-                    user_calculated_rate = user_calculated_rate.append({'movieId':ii, 'rate':rate},ignore_index=True)
+                    line_df = pd.DataFrame({'movieId':ii, 'rate':rate})
+                    user_calculated_rate = pd.concat([user_calculated_rate,line_df])
+                    # user_calculated_rate = user_calculated_rate.append({'movieId':ii, 'rate':rate},ignore_index=True)
                     
                 sorted = user_calculated_rate.sort_values(by = ['rate'], ascending=False)
                 sorted_head = sorted.head(self.k)
